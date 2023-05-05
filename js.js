4c3cd5cd59ae12db7e548f123aec9cd199308231
Panlaptrinh <128304309+Panlaptrinh@users.noreply.github.com>
2023-05-05 15:53:05 +0700

    Delete js.js

--- js.js
+++
@@ -1,6 +0,0 @@
-const linksTable = document.getElementById("linksTable");
-const newRow = linksTable.insertRow(-1);
-const websiteCell = newRow.insertCell(0);
-const linkCell = newRow.insertCell(1);
-websiteCell.innerHTML = "GitHub";
-linkCell.innerHTML = "<a href='https://www.github.com'>https://www.github.com</a>";
